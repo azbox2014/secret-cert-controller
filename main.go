70f04d32bb06de7ab7fed8a7492227e54ffc4ea2
Liqiang Zhang <liqiang.zhang.m@hotmail.com>
2026-04-28 09:20:39 +0000

    添加必要的日志打印

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/klog/v2"
 
 	ctrl "sigs.k8s.io/controller-runtime"
 
@@ -13,24 +14,37 @@ import (
 )
 
 func main() {
+	klog.Info("Starting secret-cert-controller...")
+
 	scheme := runtime.NewScheme()
 	utilruntime.Must(corev1.AddToScheme(scheme))
+	klog.Info("Kubernetes schemes registered successfully")
 
+	klog.Info("Creating controller manager...")
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme: scheme,
 	})
 	if err != nil {
+		klog.Errorf("Failed to create controller manager: %v", err)
 		os.Exit(1)
 	}
+	klog.Info("Controller manager created successfully")
 
+	klog.Info("Setting up SecretReconciler...")
 	if err = (&controllers.SecretReconciler{
 		Client: mgr.GetClient(),
 		Scheme: scheme,
 	}).SetupWithManager(mgr); err != nil {
+		klog.Errorf("Failed to setup SecretReconciler: %v", err)
 		os.Exit(1)
 	}
+	klog.Info("SecretReconciler setup completed successfully")
 
+	klog.Info("Starting controller manager...")
 	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
+		klog.Errorf("Failed to start controller manager: %v", err)
 		os.Exit(1)
 	}
+	
+	klog.Info("Controller manager stopped gracefully")
 }
